2e5ef11a6c5ee609b508fd1d235cee36e65a035b
JuanVinicius <juanvinicius14.jv@gmail.com>
2023-11-09 20:26:44 -0300

    Primeira versão do programa

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JuanVinicius/Monitorador
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Douglas"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi", comando)
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+	default:
+		fmt.Println("Não conheço este comando")
+	}
+}
