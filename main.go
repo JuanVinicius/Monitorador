6cc3f133f33b2951f85f96c30f99d64eef8b436b
JuanVinicius <juanvinicius14.jv@gmail.com>
2023-11-10 10:03:28 -0300

    Alterando partes do código para funções

--- main.go
+++ main.go
@@ -1,29 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	nome := "Douglas"
-	versao := "1.1.1"
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
+	introducao()
+	menu()
+	opcao := comando()
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi", comando)
-
-	switch comando {
+	switch opcao {
 	case 1:
-		fmt.Println("Monitorando...")
+		fmt.Println("Iniciando monitorando...")
 	case 2:
 		fmt.Println("Exibindo Logs...")
 	case 0:
 		fmt.Println("Saindo do programa...")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
 	}
+}
+
+func introducao() {
+	nome := "Juan"
+	versao := "1.1.1"
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func comando() int {
+	var comando int
+	fmt.Scan(&comando)
+
+	return comando
+}
+
+func menu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
 }
