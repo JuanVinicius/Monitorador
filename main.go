a67570d95559c44f739b8ce144877126dfd41477
JuanVinicius <juanvinicius14.jv@gmail.com>
2023-11-13 10:39:24 -0300

    Pequenas alterações e incremento da função de monitoramento

--- main.go
+++ main.go
@@ -2,29 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 
-	introducao()
-	menu()
-	opcao := comando()
+	for {
+		intro()
+		menu()
+		opcao := comando()
 
-	switch opcao {
-	case 1:
-		fmt.Println("Iniciando monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
+		switch opcao {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+		}
 	}
 }
 
-func introducao() {
+func intro() {
 	nome := "Juan"
 	versao := "1.1.1"
 	fmt.Println("Olá, sr.", nome)
@@ -42,4 +45,15 @@ func menu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Iniciando monitorando...")
+	site := "https://www.alura.com.br/"
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("O site:", site, "segue operante ")
+	} else {
+		fmt.Println("O site:", site, "Está com problemas. Status Code:", resp.StatusCode)
+	}
 }
