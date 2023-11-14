df9f6f7cd0d604ecc16301f78cfe186ebcea3ff2
JuanVinicius <juanvinicius14.jv@gmail.com>
2023-11-14 12:49:25 -0300

    Atualização da funcionalidade de monitoramento e pequenas adições

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const turnos = 3
+const delay = 5
 
 func main() {
 
+	intro()
+
 	for {
-		intro()
 		menu()
 		opcao := comando()
 
@@ -48,9 +53,25 @@ func menu() {
 }
 
 func iniciarMonitoramento() {
+	println("")
 	fmt.Println("Iniciando monitorando...")
-	site := "https://www.alura.com.br/"
+	sites := []string{"https://www.alura.com.br/", "https://www.google.com.br/"}
+
+	for i := 0; i < turnos; i++ {
+		for i, site := range sites {
+			fmt.Println("testando site", i, ":", site)
+			testaSite(site)
+		}
+		println("")
+		time.Sleep(delay * time.Second)
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
+
 	if resp.StatusCode == 200 {
 		fmt.Println("O site:", site, "segue operante ")
 	} else {
